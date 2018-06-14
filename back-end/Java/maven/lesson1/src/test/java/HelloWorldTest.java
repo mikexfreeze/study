d9af5df2cc7f9a3c0acb0414b1f486faa648a528
Xiao.Yang <yang.xiao@elanking.com>
2018-06-14 10:13:49 +0800

    hack js

---
+++ back-end/Java/maven/lesson1/src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testHello(){
+        Assert.assertEquals("Hello World!", new HelloWorld().sayHello());
+    }
+
+}
---
+++ back-end/Java/spring-boot/girl/src/main/java/com/xiao/HelloController.java
@@ -0,0 +1,21 @@
+package com.xiao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${cupSize}")
+    private String cupSize;
+
+    @Value("${age}")
+    private Integer age;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String say() {
+        return cupSize + age;
+    }
+}
