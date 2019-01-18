414a270b66a005f0aebefa5ff2522d6e00d9c7a1
Unknown <mikexfreeze@gmail.com>
2019-01-18 15:50:39 +0800

    java 学习秒杀项目 mybatis mysql连接

---
+++ back-end/Java/spring-boot-2.1.1-web-demo/src/main/java/issuesdashboard/IssuesDashboardApplication.java
@@ -0,0 +1,14 @@
+package issuesdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class IssuesDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesDashboardApplication.class, args);
+	}
+}
---
+++ back-end/Java/spring-boot-2.1.1-web-demo/src/main/java/issuesdashboard/events/GithubProjectRepository.java
@@ -0,0 +1,8 @@
+package issuesdashboard.events;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface GithubProjectRepository extends PagingAndSortingRepository<GithubProject, Long> {
+
+	GithubProject findByRepoName(String repoName);
+}
