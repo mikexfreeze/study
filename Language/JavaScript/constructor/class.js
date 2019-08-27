6f218908a758856e5fcfae3a8f5ba57c3ab01e83
Xiao <mikexfreeze@gmail.com>
2019-08-27 10:36:12 +0800

    删除React中不用的库

---
+++ Language/JavaScript/constructor/class.js
@@ -0,0 +1,12 @@
+/* create by Micheal Xiao 2019/8/20 16:17 */
+class TestConstructor {
+  constructor () {
+    console.log("constructor run")
+  }
+  
+  init () {
+    console.log("class init")
+  }
+}
+
+window.TestConstructor = TestConstructor
